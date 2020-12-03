2dfbea20ccfb1882ac6665a26a872f29de91ad9a
Gabriela Mancini <gabimancinir@gmail.com>
2020-12-02 22:05:47 -0300

    Las celdas naranjas

--- script.js
+++ script.js
@@ -5,30 +5,37 @@ fetch(url)
     .then(data => data.json())
     .then(info => {
         let infos = info.data;
-        for (let i = 0; i < infos.length; i++) {
+        for (let i = 0; i < infos.length; i++) { 
+            const tr = document.createElement('tr');
+
             /* Almacenamiento de los campos Titulo, Type, Price y Rating */
             let type = infos[i].type;
             let title = infos[i].title;
             let price = infos[i].price;
-            let rating = infos[i].rating;
-
-            /* Se remueve la categoría “bakery" */
-          
+            let rating = infos[i].rating; 
+            if(type === "bakery" ||tr.contains(title)  ){
+                tr.style.display = "none";
+            }
             /* Creación de un <tr> por cada iteración */
-            const tr = document.createElement('tr');
             tr.innerHTML = `
                 <td>${type}</td>
                 <td>${title}</td>
                 <td>${price}</td>
                 <td>${rating}</td>
             `
+            
+           
             tbody.appendChild(tr);
-            if(type === "bakery" ){
-                tr.style.display = "none";
-            }
+             /* Se remueve la categoría “bakery" */
+          
             if(type==="fruit"){
                 tr.classList.add("orange");
-            }
+            }  
+           
+           
         }
     })
-    
+    .catch (err => console.error(err));
+  
+   
+   
